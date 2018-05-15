27deef53ffbf4ae80313bcd6510663c5348fdc32
Jonathan Yu <jyu2@pandora.com>
2018-05-15 15:05:50 -0700

    Sequencer class

---
+++ app/src/main/java/com/github/improvtrainer/Sequencer.java
@@ -0,0 +1,11 @@
+package com.github.improvtrainer;
+
+import com.github.improvtrainer.model.SequencerConfiguration;
+import com.github.improvtrainer.model.Song;
+
+public class Sequencer {
+
+    public Sequencer(Song song, SequencerConfiguration configuration) {
+
+    }
+}
